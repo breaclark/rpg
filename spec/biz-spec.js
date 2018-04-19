825c9011df6ae49177a7ee9777d16105848a44fe
brea borlas and johnny ray alt <brea+johnny@>
2018-04-19 11:24:25 -0700

    checks to see if a a battle instance with player 1 and player 2 passed in as arguments is created

--- spec/biz-spec.js
+++ spec/biz-spec.js
@@ -1,11 +1,13 @@
 import { Character } from './../src/characterset.js';
 import { Ai } from './../src/ai.js';
 import { Hacks } from './../src/hacks.js';
+import { Battle } from './../src/battlesystem.js';
 // import { setClass } from './src/biz.js';
 
 describe('Character', function() {
   let player1;
   let player2;
+  let battle;
 
   beforeEach(function() {
     player1 = new Character('Johnny',1);
@@ -29,6 +31,14 @@ describe('Character', function() {
     expect(player2.characterClass.userName).toEqual('Brea');
     expect(player2.characterClass).toEqual(jasmine.any(Hacks));
 
+  });
+
+  it('should create a battle instance with player 1 and player 2 passed into it as arguments', function() {
+    battle = new Battle(player1, player2);
+    expect(battle.player1).toEqual(jasmine.any(Character));
+    expect(battle.player2).toEqual(jasmine.any(Character));
+    expect(battle.player1.userName).toEqual('Johnny');
+    expect(battle.player2.userName).toEqual('Brea');
   });
 
 });
