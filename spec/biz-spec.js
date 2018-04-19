868837778653130a87ec5e2c8efa0b88222c00b2
brea borlas and johnny ray alt <brea@>
2018-04-19 11:05:37 -0700

    Add test for player 2

--- spec/biz-spec.js
+++ spec/biz-spec.js
@@ -2,21 +2,28 @@ import { Character } from './../src/characterset.js';
 // import { setClass } from './src/biz.js';
 
 describe('Character', function() {
-  let newCharacter;
+  let player1;
+  let player2;
 
   beforeEach(function() {
-    newCharacter = new Character('Johnny', 150);
-    return newCharacter;
+    player1 = new Character('Johnny',1, 150);
+    player2 = new Character('Brea',2, 150);
+    return (player1, player2);
   });
 
   it('should create a new Character object with the name Johnny, class of hacks, and 150 health', function() {
-    expect(newCharacter.userName).toEqual('Johnny');
-    expect(newCharacter.health).toEqual(150);
+    expect(player1.userName).toEqual('Johnny');
+    expect(player1.health).toEqual(150);
   });
 
   it('should output that a hacks class was created and is set to Character.characterClass', function() {
-    newCharacter.setClass(49);
-    expect(newCharacter.characterClass.userName).toEqual('Johnny');
+    player1.setClass(50);
+    expect(player1.characterClass.userName).toEqual('Johnny');
+  });
+
+  it('should output that a hacks class was created and is set to Character.characterClass', function() {
+    player2.setClass(57);
+    expect(player2.characterClass.userName).toEqual('Brea');
   });
 
 });
