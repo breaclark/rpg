fd72624567a19e4438c6c02022a61ab881c36564
brea borlas and johnny ray alt <brea@>
2018-04-19 10:07:26 -0700

    Add new test to check setClass method

--- spec/biz-spec.js
+++ spec/biz-spec.js
@@ -1,4 +1,4 @@
-import { Character } from './../src/biz.js';
+import { Character } from './../src/characterset.js';
 // import { setClass } from './src/biz.js';
 
 describe('Character', function() {
@@ -12,7 +12,11 @@ describe('Character', function() {
   it('should create a new Character object with the name Johnny, class of hacks, and 150 health', function() {
     expect(newCharacter.userName).toEqual('Johnny');
     expect(newCharacter.health).toEqual(150);
-    newCharacter.setClass('hacks');
-    expect(newCharacter.characterClass).toEqual('hacks');
   });
+
+  it('should output that a hacks class was created and is set to Character.characterClass', function() {
+    newCharacter.setClass(49);
+    expect(newCharacter.characterClass.userName).toEqual('Johnny');
+  });
+
 });
