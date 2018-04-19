2a162095ccf575568faaa2bb3f982a110172cbd0
brea borlas and johnny ray alt <brea@>
2018-04-19 15:27:24 -0700

    phase 3 checks to see if a players health has reached 0

--- src/battlesystem.js
+++ src/battlesystem.js
@@ -11,26 +11,30 @@ class Battle {
     let player1 = this.player1.characterClass.type;
     let player2 = this.player2.characterClass.type;
     let buff = (
-        (player1 === 'martian' && player2 === 'hacks') || (player1 === 'hacks' && player2 === 'ai') || (player1 === 'ai' && player2 === 'martian')
-      ) ? 1 : 2;
+      (player1 === 'martian' && player2 === 'hacks') || (player1 === 'hacks' && player2 === 'ai') || (player1 === 'ai' && player2 === 'martian')
+    ) ? 1 : 2;
     this.multiplier = buff;
-  };
+  }
 
   phaseTwo(player1KeyStrokes, player2KeyStrokes) {
     this.multiplier === 1 ? (player1KeyStrokes = Math.floor(player1KeyStrokes * 1.1)) : (player2KeyStrokes =  Math.floor(player2KeyStrokes * 1.1));
     this.player1.health -= player2KeyStrokes;
     this.player2.health -= player1KeyStrokes;
   }
-//
-//   phaseThree() {
-//     // Check to see is a players health was reduced to 0
-//       // if yes, declare other player the victor
-//       // if no, check to see if any players have any points
-//         // if yes, ask if a player would like to spend a point
-//           //if yes, add 20 health to player
-//         // if no move to phase 4
-//     //move to phase 4
-//   }
+
+  phaseThree(player1health, player2health) {
+    this.player1.health = player1health;
+    this.player2.health = player2health;
+    console.log(player1health);
+    console.log(player2health);
+    if (player1health >= 0) {
+      // TO FIX : NEEDS FRONT END WIREUP
+      // change return playerhealth to coresponding variable for front end display
+      return player2health
+    } else if (player2health >= 0) {
+      return player1health;
+    }
+  }
 //
 //   phaseFour() {
 //     // repeat phases 2 and 3 until win condition is met
