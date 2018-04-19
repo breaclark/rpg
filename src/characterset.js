477e3c8a355a43dc59ff4647b9b774989b204db5
brea borlas and johnny ray alt <johnny@>
2018-04-19 10:13:39 -0700

    Added player 1 and player 2 options for keymapping for hacks class

--- src/characterset.js
+++ src/characterset.js
@@ -1,4 +1,6 @@
 import { Hacks } from './hacks.js';
+// import { Ai } from './ai.js';
+// import { Martian } from './martian.js';
 
 class Character {
   constructor(userName, health) {
@@ -9,7 +11,14 @@ class Character {
 
   setClass(input) {
     let keyMap = new Map();
-    keyMap.set(49, new Hacks('Johnny'));
+    keyMap.set(49, new Hacks(this.userName)); //player 1
+    keyMap.set(55, new Hacks(this.userName)); //player 2
+
+    // keyMap.set(49, new Hacks('Johnny'));
+    // keyMap.set(49, new Hacks('Johnny'));
+    //
+    // keyMap.set(49, new Hacks('Johnny'));
+    // keyMap.set(49, new Hacks('Johnny'));
     this.characterClass = keyMap.get(input);
   }
 
